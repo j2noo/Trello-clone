58636522163a1ba1e298e39e1e78955be59e4f98
j2noo <wlsdndml213@naver.com>
2023-07-26 14:18:48 +0900

    chore : CRA

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { styled } from "styled-components";
+
+const Test = styled.div``;
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
