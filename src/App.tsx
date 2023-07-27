150e7a3fac7e56fdf43b5677db1f354b1f06bdad
j2noo <wlsdndml213@naver.com>
2023-07-27 14:09:59 +0900

    feat : 드래그앤드롭 & provided.placeholder 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,60 @@ import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { styled } from "styled-components";
 
-function App() {
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+const Board = styled.div`
+  padding: 30px 10px 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px 10px;
+  margin-bottom: 5px;
+`;
 
+const toDos = ["A", "B", "C", "D", "E"];
+
+function App() {
   const onDragEnd = () => {};
   return (
     <>
       {/* <GlobalStyle></GlobalStyle> */}
       <DragDropContext onDragEnd={onDragEnd}>
-        <div>
-          <Droppable droppableId="one">
-            {(provided) => (
-              <ul ref={provided.innerRef} {...provided.droppableProps}>
-                <Draggable draggableId="first" index={0}>
-                  {(provided) => (
-                    <li ref={provided.innerRef} {...provided.draggableProps}>
-                      <span {...provided.dragHandleProps}>🔥</span>
-                      One
-                    </li>
-                  )}
-                </Draggable>
-                <Draggable draggableId="second" index={1}>
-                  {(provided) => (
-                    <li ref={provided.innerRef} {...provided.draggableProps}>
-                      <span {...provided.dragHandleProps}>🔥</span>
-                      Two
-                    </li>
-                  )}
-                </Draggable>
-              </ul>
-            )}
-          </Droppable>
-        </div>
+        <Wrapper>
+          <Boards>
+            <Droppable droppableId="one">
+              {(provided) => (
+                <Board ref={provided.innerRef} {...provided.droppableProps}>
+                  {toDos.map((toDo, index) => (
+                    <Draggable draggableId={toDo} index={index}>
+                      {(provided) => (
+                        <Card ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                          {toDo}
+                        </Card>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </Board>
+              )}
+            </Droppable>
+          </Boards>
+        </Wrapper>
       </DragDropContext>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { darkTheme } from "./theme";
+
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
