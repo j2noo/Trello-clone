2d2226ede2bcb55d3e1434917ec6280df7249c42
j2noo <wlsdndml213@naver.com>
2023-07-27 17:15:34 +0900

    feat : google font 설정

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,13 @@ const Card = styled.div`
   margin-bottom: 5px;
 `;
 
-const toDos = ["A", "B", "C", "D", "E"];
+const toDos = [
+  "모든 인류 구성원의 천부의 존엄성과 동등",
+  "폰트에서 원하는 폰트를 가져온다",
+  "index.tsx에서 React.StrictMode를 div로 변경",
+  "DWhereas a common understanding",
+  "E",
+];
 
 function App() {
   const onDragEnd = () => {};
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { darkTheme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+/* @import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Noto+Sans+KR:wght@400;500;700;900&display=swap'); */
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -58,8 +58,8 @@ table {
   box-sizing: border-box;
 }
 body {
-  font-weight: 300;
-  font-family: 'Source Sans Pro', sans-serif;
+  font-weight: 400;
+  font-family: 'Dongle', sans-serif;
   background-color: ${(props) => props.theme.bgColor};
   color:black;
   line-height: 1.2;
