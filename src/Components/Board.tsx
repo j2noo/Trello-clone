f920cd000f01c89bb987b5096458e6303a6f7b7e
j2noo <wlsdndml213@naver.com>
2023-07-29 15:50:13 +0900

    bug : custom props 경고 해결

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -2,6 +2,7 @@ import { Droppable } from "react-beautiful-dnd";
 import DraggableCard from "./DrabbleCard";
 import { styled } from "styled-components";
 import { useRef } from "react";
+import { useForm } from "react-hook-form";
 
 const Wrapper = styled.div`
   padding-top: 10px;
@@ -18,41 +19,57 @@ const Title = styled.div`
 `;
 const Area = styled.div<IAreaProps>`
   background-color: ${(props) =>
-    props.isDraggingOver ? "#dfe6e9" : props.isDraggingFromThis ? "#b2bec3" : "transparent"};
+    props.$isDraggingOver
+      ? "#dfe6e9"
+      : props.$isDraggingFromThis
+      ? "#b2bec3"
+      : "transparent"};
   flex-grow: 1;
   transition: background-color 0.3s ease-in-out;
   padding: 20px;
+`;
+const Form = styled.form`
+  width: 100%;
 `;
 interface IBoardProps {
   toDos: string[];
   boardId: string;
 }
 interface IAreaProps {
-  isDraggingFromThis: boolean;
-  isDraggingOver: boolean;
+  $isDraggingFromThis: boolean;
+  $isDraggingOver: boolean;
+}
+// form의 입력타입?
+interface IForm {
+  toDo: string;
 }
 function Board({ toDos, boardId }: IBoardProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const onClick = () => {
-    inputRef.current?.focus();
-    console.log(inputRef);
-  };
+  const { register, setValue, handleSubmit } = useForm<IForm>();
 
   return (
     <Wrapper>
       <Title>{boardId}</Title>
-      <input ref={inputRef} type="text" />
-      <button onClick={onClick}>click me</button>
+      <Form>
+        <input
+          type="text"
+          placeholder={`${boardId}에 추가하세요`}
+          {...(register("toDo"), { required: true })}
+        ></input>
+      </Form>
       <Droppable droppableId={boardId}>
         {(provided, snapshot) => (
           <Area
-            isDraggingOver={snapshot.isDraggingOver}
-            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            $isDraggingOver={snapshot.isDraggingOver}
+            $isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
             ref={provided.innerRef}
             {...provided.droppableProps}
           >
             {toDos.map((toDo, index) => (
-              <DraggableCard key={toDo} toDo={toDo} index={index}></DraggableCard>
+              <DraggableCard
+                key={toDo}
+                toDo={toDo}
+                index={index}
+              ></DraggableCard>
             ))}
             {provided.placeholder}
           </Area>
--- src/Components/DrabbleCard.tsx
+++ src/Components/DrabbleCard.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { styled } from "styled-components";
-const Card = styled.div<{ isDragging: boolean }>`
-  background-color: ${(props) => (props.isDragging ? "#74b9ff" : props.theme.cardColor)};
+const Card = styled.div<{ $isDragging: boolean }>`
+  background-color: ${(props) => (props.$isDragging ? "#74b9ff" : props.theme.cardColor)};
   padding: 10px 10px;
   margin-bottom: 5px;
   border-radius: 5px;
-  box-shadow: ${(props) => (props.isDragging ? "0px 2px 10px rgba(0, 0, 0, 0.5)" : "none")};
+  box-shadow: ${(props) => (props.$isDragging ? "0px 2px 10px rgba(0, 0, 0, 0.5)" : "none")};
 `;
 
 interface IDraggableCardProps {
@@ -18,7 +18,7 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
     <Draggable key={toDo} draggableId={toDo} index={index}>
       {(provided, snapshot) => (
         <Card
-          isDragging={snapshot.isDragging}
+          $isDragging={snapshot.isDragging}
           ref={provided.innerRef}
           {...provided.dragHandleProps}
           {...provided.draggableProps}
